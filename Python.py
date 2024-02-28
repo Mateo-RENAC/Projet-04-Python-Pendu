b26a3203d808d31868fe92a64e5b5eb94dcd4bb0
CESIMateoRENAC <mateo.renac@viacesi.fr>
2024-02-28 12:09:43 +0100

    Quelques changement + création de la méthode GetListeLettreEntree

--- Python.py
+++ Python.py
@@ -1,9 +1,11 @@
 class Game :
 
     def __init__(self) :
+        '''Initialise les variables principale du jeux'''
         self.Pseudo = None
         self.Mot = None
-        self.Listunderscore = None
+        self.Listunderscore = []
+        self.Listelettre = []
         return
 
     def DemandePseudo(self):
@@ -11,33 +13,44 @@ class Game :
         Demande à l'utilisateur
         un Pseudonyme et le met 
         dans la variable Pseudo'''
+
         self.Pseudo = input("Choissisez un Pseudonyme :\n")
         return
     
-    def underscore(self, motacacher):
+    def Underscore(self, motacacher):
         '''underscore(motcacher)
         prend en paramètre une 
         chaine de caractère et 
         la transforme en une 
         liste de underscore de 
         même taille'''
+
         taillemot = len(motacacher)
-        self.Listunderscore = []
         for i in range(taillemot):
             self.Listunderscore.append("_")
         self.Listunderscore = ''.join(self.Listunderscore)
         print(self.Listunderscore)
         return self.Listunderscore
 
-    
+
     def ChoixMot(self):
+        '''Demande a l'utilisateur le mot qui devra être trouvé'''
         self.Mot = input("Choissisez un mot \n")
         return
-    
-    def getMot(self) :
+
+
+    def GetListeLettreEntree(self) :
+        '''retourne la liste des lettre déjà entrée'''
+        return self.Listelettre
+
+
+    def GetMot(self) :
+        '''retourne le mot qui doit être trouvé'''
         return self.Mot
-    
-    def getPseudo(self) :
+
+
+    def GetPseudo(self) :
+        '''retourne le pseudonyme de l'utilisateur'''
         return self.Pseudo
     
     
@@ -46,7 +59,7 @@ class Game :
     
 Pendu = Game()
 Pendu.ChoixMot()
-Pendu.underscore(Pendu.getMot())
+Pendu.Underscore(Pendu.GetMot())
 
 
 
