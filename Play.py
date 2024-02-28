eb5bc8509cb8520eaef26c136f79611d37f966a6
CESIMateoRENAC <mateo.renac@viacesi.fr>
2024-02-28 14:00:04 +0100

    Fresh code

--- Play.py
+++ Play.py
@@ -7,20 +7,27 @@ def Play() :
     Pendu.ChoixMot()
     Pendu.Underscore(Pendu.mot)
     essais = 0
+
     while essais < 5 and Pendu.GetMot() != Pendu.GetUnderscore() :
         print(Pendu.GetUnderscore())
+        
         if len(Pendu.GetListeLettreEntree()) < 0 :
             print(Pendu.GetListeLettreEntree())
         print("Nombre d'essais : " ,essais)
         lettreessayer = input("entrez une lettre \n")
+
         lettreessayer = str(lettreessayer)
+
         if Pendu.EstdansListLettreEntree(lettreessayer) :
             print("Déjà rentrée essayez une autre lettre")
+
         elif Pendu.EstDansLeMot(lettreessayer,Pendu.mot) == False :
             essais += 1
+
     if Pendu.GetMot() == Pendu.GetUnderscore() :
         print("Victoire ! le mot était bien : " + Pendu.mot)
         return
+    
     else :
         print("Pendu ! le mot était : " + Pendu.mot)
         return
