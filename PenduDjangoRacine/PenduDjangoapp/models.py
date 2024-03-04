8e2dc1065ab03803ab00d708d34d3f05c8ed45a4
CESIMateoRENAC <mateo.renac@viacesi.fr>
2024-03-04 17:14:45 +0100

    Supprimer Django

--- PenduDjangoRacine/PenduDjangoapp/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class ModelScore(models.Model):
-    id = models.AutoField(primary_key=True)
-    nom = models.CharField(max_length=20)
-    win = models.CharField(max_length=5, choices=[('oui', 'Oui'), ('non', 'Non')])
-
