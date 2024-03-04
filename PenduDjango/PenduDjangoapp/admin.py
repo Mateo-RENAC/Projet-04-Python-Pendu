3a45c89a0c283c842237dbf73d51fa11ad34c6ae
CESIMateoRENAC <mateo.renac@viacesi.fr>
2024-03-04 08:51:15 +0100

    Création et intégration du modèle de base de donnée ModelScore

--- PenduDjango/PenduDjangoapp/admin.py
+++ PenduDjango/PenduDjangoapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import ModelScore
+
+admin.site.register(ModelScore)
--- PenduDjango/PenduDjangoapp/models.py
+++ PenduDjango/PenduDjangoapp/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class VotreModele(models.Model):
+class ModelScore(models.Model):
     id = models.AutoField(primary_key=True)
     nom = models.CharField(max_length=20)
     win = models.CharField(max_length=5, choices=[('oui', 'Oui'), ('non', 'Non')])
